dc83ac9594b3fdfec17e7c97e40df6be916f92c9
Harshul <67414976+Harshul-18@users.noreply.github.com>
2022-08-25 10:24:41 +0530

    Update onlineStatsViewer.py

--- onlineStatsViewer.py
+++ onlineStatsViewer.py
@@ -17,20 +17,13 @@ hideStreamlitStyle = """
     <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
+    body {
+        background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+        background-size: cover;
+    }
     </style>
 """
 st.markdown(hideStreamlitStyle, unsafe_allow_html=True)
-
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
 
 with st.sidebar:
 
